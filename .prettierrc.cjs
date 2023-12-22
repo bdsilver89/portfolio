5b404c58abade69fc87af44cb09e5f658f15fab0
Brian Silver <github@briansilver.net>
2023-12-22 12:13:13 -0500

    feat: update prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,10 @@
 module.exports = {
   plugins: ["prettier-plugin-tailwindcss"],
   tailwindFunctions: ["clsx"],
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  jsxSingleQuote: false,
+  endOfLine: "lf",
 };
